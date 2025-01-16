45e015939eb27b2225696f9e4a2be733cbb46aed
Andres Zelcer <45225345+azelcer@users.noreply.github.com>
2025-01-16 11:59:06 -0300

    Split qt utils

--- src/takyaq/frontends/PyQt_frontend.py
+++ src/takyaq/frontends/PyQt_frontend.py
@@ -36,6 +36,7 @@ from PyQt5.QtWidgets import (
     QDoubleSpinBox,
 )
 from PyQt5.QtGui import QDoubleValidator
+from .qt_utils import create_spin as _create_spin, GroupedCheckBoxes
 import pyqtgraph as _pg
 
 import logging as _lgn
@@ -68,40 +69,6 @@ _NPY_Z_DTYPE = _np.dtype([
     ])
 
 
-class GroupedCheckBoxes:
-    """Manages grouped CheckBoxes states.
-
-    This is a helper class to ease GUI implementation. It implements an 'All'
-    checkbox that controls and stays sinchronized with others. It should be used
-    carefully to avoid state changes loops.
-    """
-
-    def __init__(self, all_checkbox: QCheckBox, *other_checkboxes):
-        """Init class.
-
-        Parameters
-        ----------
-        all_checkbox : QCheckBox
-            The checkbox that checks/unchecks all others.
-        *other_checkboxes : Iterable[QCheckBox]
-            All other checkboxes.
-        """
-        self.acb = all_checkbox
-        self.others = other_checkboxes
-        for _ in other_checkboxes:
-            _.stateChanged.connect(self.on_state)
-        all_checkbox.clicked.connect(self.on_click)
-
-    def on_state(self, state: int):
-        """Handle single items change."""
-        self.acb.setChecked(all([_.isChecked() for _ in self.others]))
-
-    def on_click(self, is_checked: bool):
-        """Handle 'All' checkbox click."""
-        for _ in self.others:
-            _.setChecked(is_checked)
-
-
 class QReader(QObject):
     """Helper class to send data from stabilizar to Qt GUI.
 
@@ -116,18 +83,6 @@ class QReader(QObject):
     def cb(self, data: PointInfo):
         """Report data."""
         self.new_data.emit(data.time, data.image, data.z_shift, data.xy_shifts)
-
-
-def _create_spin(value: float, decimals: int, step: float,
-                 minimum: float = 0., maximum: float = 1.):
-    """Create spin with properties."""
-    rv = QDoubleSpinBox()
-    rv.setValue(value)
-    rv.setDecimals(decimals)
-    rv.setMinimum(minimum)
-    rv.setMaximum(maximum)
-    rv.setSingleStep(step)
-    return rv
 
 
 def save_config(config_data: dict, filename: str = _CONFIG_FILENAME):
